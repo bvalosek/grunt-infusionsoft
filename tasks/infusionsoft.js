1bc80f077b74f9f21b2bd5d8ae221962d886dec8
Brandon Valosek <bvalosek@gmail.com>
2013-08-08 00:17:29 -0500

    initial commit

---
+++ tasks/infusionsoft.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask(
+        'infusionsoft',
+        'Create classes and interfaces for Infusionsoft\'s XML-RPC API from their online documentation.', function() {
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
+            var dest = f.dest;
+
+
+        });
+
+    });
+
+};
